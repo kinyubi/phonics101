be46428ed6e42b7f619cffee5dcc3410ef260992
Danilo Jr <danuki.jr@gmail.com>
2021-06-14 12:01:21 +0800

    The EmailBody is passed onto the arguments instead of separate fields. TrainerCode is retrieved to be able to send the email to the trainer (CC'd)

--- src/ReadXYZ/Handlers/ParentEmail.php
+++ src/ReadXYZ/Handlers/ParentEmail.php
@@ -8,6 +8,7 @@ use App\ReadXYZ\Data\StudentsData;
 use App\ReadXYZ\Helpers\PhonicsException;
 use App\ReadXYZ\Helpers\Util;
 use App\ReadXYZ\Twig\LessonListTemplate;
+use App\ReadXYZ\Data\TrainersData;
 use \Throwable;
 
 class ParentEmail extends AbstractHandler
@@ -15,26 +16,16 @@ class ParentEmail extends AbstractHandler
     private static string $email = <<<EOT
     <html lang="en">
     <head>
-    <title>Congratulations</title>
-    <link rel="stylesheet" href="{{phonicsUrl}}/css/bootstrap4-custom/bootstrap.min.css">
-    <link rel="stylesheet" href="{{phonicsUrl}}/css/colorbox/colorbox.css" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{phonicsUrl}}/css/phonics.css">
+        <style>
+            @font-face {
+                font-family: "Muli";
+                src: url(https://fonts.googleapis.com/css?family=Muli);
+            }
+        </style>
     </head>
     <body>
-    <div class="container-fluid bg-white" >
-        <div class="row  d-flex flex-nowrap  m-2 justify-content-between">
-            <div class="col-8" style="font-size: 1em;font-family:'Comic Sans MS',serif;">
-                <p class="my-2 font-weight-bold">Hi {{parent}},</p>
-                <p>Today {{student}} worked on the lesson <span>"{{lesson}}"</span>.</p><br>
-                <p class="font-weight-bold">The practice list is:</p>
-                <ul class="ml-3">{{words}}</ul>
-                <br><br>
-<!--                <p class="font-weight-bold">The fluency passages are:</p>-->
-                <ul>{{sentences}}</ul>
-                <br>
-            </div>
-        </div>
+    <div class="container" style="width: 100%;">
+        <pre class="email-content" style="font-family: sans-serif; font-size: 1.2em;">{{emailBody}}</pre>
     </div>
 EOT;
 
@@ -47,30 +38,38 @@ EOT;
 
         self::fullLocalErrorReportingOn();
         try {
+            $trainerCode = $_REQUEST['trainerCode'];
             $studentCode = $_REQUEST['studentCode'];
             $emailOnFile = $studentsData->getParentEmail($studentCode);
             $to          = $_REQUEST['parentEmail'];
-            $subject     = 'Congratulations';
-            $parentName  = $_REQUEST['parentName'];
-            $studentName = $_REQUEST['studentName'];
-            $lessonName  = $_REQUEST['lessonName'];
-            $words       = $_REQUEST['words'];
-            $sentences   = $_REQUEST['sentences'];
-            $wordlist    = '';
-            foreach ($words as $word) {
-                $wordlist .= "<li class='ml-3'>$word</li>";
-            }
-            $sentenceList = '';
-            foreach ($sentences as $sentence) {
-                $sentenceList .= "<li class='ml-3 mt-2'>$sentence</li>";
-            }
+            $subject     = 'Lesson Summary';
+            $emailBody   = $_REQUEST['emailBody'];
+            $trainersData = new TrainersData();
+            $trainer = $trainersData->get($trainerCode);
+
+            // $parentName  = $_REQUEST['parentName'];
+            // $studentName = $_REQUEST['studentName'];
+            // $lessonName  = $_REQUEST['lessonName'];
+            // $words       = $_REQUEST['words'];
+            // $sentences   = $_REQUEST['sentences'];
+            // $wordlist    = '';
+            // foreach ($words as $word) {
+            //     $wordlist .= "<li class='ml-3'>$word</li>";
+            // }
+            // $sentenceList = '';
+            // foreach ($sentences as $sentence) {
+            //     $sentenceList .= "<li class='ml-3 mt-2'>$sentence</li>";
+            // }
             $phonicsUrl = Util::getPublicPath();
-            $vars       = ['{{phonicsUrl}}', '{{parent}}', '{{student}}', '{{lesson}}', '{{words}}', '{{sentences}}'];
-            $values     = [$phonicsUrl, $parentName, $studentName, $lessonName, $wordlist, $sentenceList];
-            $replaceCount = 0;
+            // $vars       = ['{{phonicsUrl}}', '{{parent}}', '{{student}}', '{{lesson}}', '{{words}}', '{{sentences}}'];
+            // $values     = [$phonicsUrl, $parentName, $studentName, $lessonName, $wordlist, $sentenceList];
+            // $replaceCount = 0;
+            $vars = ['{{phonicsUrl}}', '{{emailBody}}'];
+            $values = [$phonicsUrl, $emailBody];
             $message    = str_replace($vars, $values, self::$email, $replaceCount);
             $headers    = "MIME-Version: 1.0\r\nContent-type:text/html;charset=UTF-8\r\n";
             $headers    .= 'From: <noreply@readxyz.com>' . "\r\n";
+            $headers    .= 'Cc: ' . $trainer->userName . '\r\n';
             if (empty($to)) {
                 Util::redBox("An email address must be provided.");
             } else {
