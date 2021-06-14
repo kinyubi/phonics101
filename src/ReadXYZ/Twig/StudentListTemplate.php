bf905c31bf390f2645e1ee4cb53350c4b6893445
Danilo Jr <danuki.jr@gmail.com>
2021-06-14 12:15:11 +0800

    Added BreadCrumb logic

--- src/ReadXYZ/Twig/StudentListTemplate.php
+++ src/ReadXYZ/Twig/StudentListTemplate.php
@@ -5,8 +5,11 @@ namespace App\ReadXYZ\Twig;
 
 
 use App\ReadXYZ\Data\Views;
+use App\ReadXYZ\Data\TrainersData;
 use App\ReadXYZ\Helpers\PhonicsException;
 use App\ReadXYZ\Page\Page;
+use App\ReadXYZ\Models\BreadCrumbs;
+use App\ReadXYZ\Models\Session;
 
 class StudentListTemplate
 {
@@ -16,6 +19,17 @@ class StudentListTemplate
      */
     public function display(): void
     {
+
+        $currentCrumb = 'students';
+        $crumbs = (new BreadCrumbs())->getPrevious($currentCrumb);
+
+
+        $trainerCode = Session::getTrainerCode();
+        $trainersData = new TrainersData();
+        $trainer = $trainersData->get($trainerCode);
+        $displayName = ucfirst($trainer->displayName);
+
+
         $allStudents = Views::getInstance()->getMapOfStudentsForUser();
         $page = new Page('Select a student');
         $studentLinks = [];
@@ -25,7 +39,17 @@ class StudentListTemplate
                 'title' => ucfirst($name)
             ];
         }
-        $args = ['page' => $page, 'studentLinks' => $studentLinks];
+        $args = [
+            'page' => $page,
+            'studentLinks' => $studentLinks,
+            'displayName' => $displayName,
+            'this_crumb' => $currentCrumb
+        ];
+
+        if (!empty($crumbs)) {
+            $args['previous_crumbs'] = $crumbs;
+        }
+
         echo TwigFactory::getInstance()->renderTemplate('students.html.twig', $args);
     }
 
